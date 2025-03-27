a4e5d500ae32e9c71478a8fad99e9d9720970b07
directicket <mathew.com.com@gmail.com>
2025-03-27 01:40:51 +0100

    countdown bg green

--- app/(root)/welcome/page.tsx
+++ app/(root)/welcome/page.tsx
@@ -4,7 +4,7 @@ const EventPage = () => {
   const eventDate = new Date("2025-03-28T00:00:00Z").getTime(); // Set your event date here
 
   return (
-    <div className='text-black bg-lime-400 flex flex-col items-center justify-center h-screen text-center'>
+    <div className='text-black bg-lime-500 flex flex-col items-center justify-center h-screen text-center'>
       <h1 className='p-20-regular'>START SELLING TONIGHT</h1>
       <Countdown targetDate={eventDate} />
     </div>
