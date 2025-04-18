28ec8beae39d55cd270ad373acb77d77c45d59a2
directicket <mathew.com.com@gmail.com>
2025-04-18 16:22:03 +0100

    revert no usernmae on scan

--- app/(root)/validate/[finalCode]/page.tsx
+++ app/(root)/validate/[finalCode]/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 
 const ValidateQR = ({ params }: { params: { finalCode: string } }) => {
   const [validationStatus, setValidationStatus] = useState<string | null>(null);
-  const [user, setUser] = useState<any>(null); // State to store user data
 
   useEffect(() => {
     const validateQRCode = async () => {
@@ -14,7 +13,6 @@ const ValidateQR = ({ params }: { params: { finalCode: string } }) => {
 
         if (data.valid) {
           setValidationStatus("QR Code validated successfully.");
-          setUser(data.user); // Set the user data from the API response
         } else {
           setValidationStatus("QR Code is invalid or has already been used.");
         }
@@ -30,18 +28,8 @@ const ValidateQR = ({ params }: { params: { finalCode: string } }) => {
   return (
     <div className="flex flex-col gap-2 text-white items-center justify-center text-center h-screen">
       <div className='self-center'>
-        <h1 className='h3-bold tracking'>Validator</h1>
+        <h1 className='h3-bold tracking'>validator</h1>
         <p>{validationStatus ? validationStatus : 'Please wait, Ticket is being checked...'}</p>
-        
-        {/* Conditionally render user info if available */}
-        {user && (
-          <div className="mt-4">
-            <h2 className="text-xl">Ticket holder details:</h2>
-            <p>Name: {user.name}</p>
-            <p>Email: {user.email}</p>
-            {/* Add any other user data you want to display */}
-          </div>
-        )}
       </div>
     </div>
   );
